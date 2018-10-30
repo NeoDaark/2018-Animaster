fc724c8ad7c4d0ec9c295fa1ca32d0fdffcfdc23
NeoDaark <DaarkAssassin@gmail.com>
2018-10-30 14:54:45 +0100

    task dropdown deleted

--- Animaster/Public/layouts/menu.php
+++ Animaster/Public/layouts/menu.php
@@ -225,68 +225,6 @@
 				</li>
 
 				<li class="dropdown hidden-xs-down" <?=$icons?>>
-						<a class="waves-effect" href="#" data-toggle="dropdown"><i class="zmdi zmdi-check-circle"></i></a>
-
-						<div class="dropdown-menu dropdown-menu-right dropdown-menu--block" role="menu">
-								<div class="listview listview--hover">
-										<div class="listview__header">Tasks</div>
-
-										<a href="#" class="listview__item">
-												<div class="listview__content">
-														<div class="listview__heading">HTML5 Validation Report</div>
-
-														<div class="progress">
-																<div class="progress-bar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-														</div>
-												</div>
-										</a>
-
-										<a href="#" class="listview__item">
-												<div class="listview__content">
-														<div class="listview__heading">Google Chrome Extension</div>
-
-														<div class="progress">
-																<div class="progress-bar bg-warning" style="width: 43%" aria-valuenow="43" aria-valuemin="0" aria-valuemax="100"></div>
-														</div>
-												</div>
-										</a>
-
-										<a href="#" class="listview__item">
-												<div class="listview__content">
-														<div class="listview__heading">Social Intranet Projects</div>
-
-														<div class="progress">
-																<div class="progress-bar bg-success" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-														</div>
-												</div>
-										</a>
-
-										<a href="#" class="listview__item">
-												<div class="listview__content">
-														<div class="listview__heading">Bootstrap Admin Template</div>
-
-														<div class="progress">
-																<div class="progress-bar bg-info" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-														</div>
-												</div>
-										</a>
-
-										<a href="#" class="listview__item">
-												<div class="listview__content">
-														<div class="listview__heading">Youtube Client App</div>
-
-														<div class="progress">
-																<div class="progress-bar bg-danger" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-														</div>
-												</div>
-										</a>
-
-										<a href="#" class="view-more">View all tasks</a>
-								</div>
-						</div>
-				</li>
-
-				<li class="dropdown hidden-xs-down" <?=$icons?>>
 						<a class="waves-effect" href="#" data-toggle="dropdown"><i class="zmdi zmdi-apps"></i></a>
 
 						<div class="dropdown-menu dropdown-menu-right dropdown-menu--block" role="menu">
